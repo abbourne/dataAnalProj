6acce91071a3fa33f87f005397bb2d53a91394cd
abbourne <wbbourne@gmail.com>
2026-01-07 11:34:50 -0500

    Initial commit after setting up a basic package template

---
+++ src/dataanalproj/__init__.py
@@ -0,0 +1,13 @@
+# __init__.py flags that this directory is a package
+# You can put package-level initialization code here if needed.
+# You can also import key functions or classes here to make them
+# accessible directly from the package.
+
+# Example version number This can be used to declare the package version
+__version__ = "0.1.0"
+
+# You can use also use  ____init____.py to run a main() function, instead of using __main__.py
+# However, using __main__.py is more common for packages.
+# from dataanalproj.main import main
+# if __name__ == "__main__":
+#     main()
